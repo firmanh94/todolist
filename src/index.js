5b2f1625b2ce970d1c1f90269ae04b543c403fdc
firmanh94 <fir.hadi13@gmail.com>
2022-06-23 02:12:34 +0700

    Added my project

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from './App';
 
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
 ];
 
 ReactDOM.render(
